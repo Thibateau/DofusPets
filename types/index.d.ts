d6c9b2c3d0b4603cb9766a0f97ea32d1e3b065a4
T-boot <thibateau@gmail.com>
2022-03-08 11:58:45 +0100

    types the pets data

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,10 @@
-export type TLoginForm = {
-	username: string
-	password: string
+export type ListPet= {
+	_id: number
+	ankamaId: number
+	name: string
+	level: number
+	type: string
+	imgUrl: string
+	url: string
+	description: string 
 }
