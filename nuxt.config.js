fa05d1e3900ba1c342d8c89ee4864b9db060d3bd
T-boot <thibateau@gmail.com>
2022-03-10 11:40:57 +0100

    name of the app changed

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'DofusPets',
+    title: 'RandomSimpsons',
     htmlAttrs: {
       lang: 'en'
     },
