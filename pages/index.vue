a633d1d57d06fa88a7220308164580126ca0ba16
Alexandre Switalski <alexandre.swit@gmail.com>
2022-03-10 11:15:34 +0100

    feat(store): impl vuex

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
 <div class="bg-blue-400">
   <div class="min-h-screen min-w-screen xl:py-24 xl:px-72  xl:pb-12 rounded-full bg-white shadow-lg shadow-black ring ring-black ring-offset-8 ring-offset-red-600">
-      <Simpsons v-if="simpsonCharacter" class="":simp="simpsonCharacter" />
-      <div v-if="simpsonCharacter===undefined">
+      <Simpsons v-if="simp" class="" :simp="simp" />
+      <div v-if="simp===undefined">
         <img src="~assets/images/homer-simpson-fin.jpg">
         <h2 class="text-l font-bold xl:text-5xl"> Houu pinaize ca bug... </h2>
-      </div>  
+      </div>
       <div class="flex flex-col">
         <button @click="randomSimpsons" class="mx-auto flex justify-center">
-        <div class="xl:h-auto xl:w-60 h-auto w-36 mx-auto pt-8"> 
+        <div class="xl:h-auto xl:w-60 h-auto w-36 mx-auto pt-8">
           <img
           :class="[{'animate-bounce':loading}]"
           src="~assets/images/pink-donuts.png"
@@ -24,25 +24,20 @@
 <script lang="ts">
 import { SimpsonsCharacter } from "~/types";
 export default {
-  data() {
-    return {
-      simpsonCharacter: null as SimpsonsCharacter | null | undefined,
-      loading: false,
-    };
-  },
-  async asyncData({ $simpsonsApi }) {
-    const result = await $simpsonsApi.getList();
-    return {
-      simpsonCharacter: result?.data?.[0],
-    };
-  },
-  methods: {
-    async randomSimpsons() {
-      this.loading = true
-      const result = await this.$simpsonsApi.getList();
-      this.simpsonCharacter = result?.data?.[0];
-      this.loading = false
+  computed: {
+    simp () {
+      console.log('this.$store.getters', this.$store.getters)
+      return this.$store.getters.getSimpsonCharater
     },
+    loading () {
+      return this.$store.getters.getLoading
+    }
+  },
+
+  methods: {
+   async randomSimpsons() {
+     await this.$store.dispatch('getSimpsonCharacter')
+   }
   },
 };
 </script>
--- store/index.ts
+++ store/index.ts
@@ -1,3 +1,41 @@
-const state = ({
-  pets: []
+import { SimpsonsCharacter } from '@/types'
+
+export const state = () => ({
+  simpsonCharacter: null,
+  loading: false
 })
+
+export const getters = {
+  getSimpsonCharater(state) {
+    return state.simpsonCharacter;
+  },
+  getLoading(state) {
+    return state.loading
+  }
+}
+
+export const mutations = {
+ SET_SIMPSON_CHARACTER(state, simpson: SimpsonsCharacter) {
+   state.simpsonCharacter = simpson
+ },
+ SET_LOADING(state, isLoading) {
+   state.loading = isLoading
+ }
+}
+
+
+export const actions = {
+  async getSimpsonCharacter({ commit, dispatch, getters }) {
+    commit('SET_LOADING', true)
+    const result = await this.$simpsonsApi.getList();
+    const simpsonCharacter = result?.data?.[0];
+    commit('SET_SIMPSON_CHARACTER', simpsonCharacter)
+    commit('SET_LOADING', false)
+    console.log('result', result)
+  },
+
+  async nuxtServerInit({ dispatch }) {
+    console.log('server init', )
+    await dispatch('getSimpsonCharacter')
+  }
+}
